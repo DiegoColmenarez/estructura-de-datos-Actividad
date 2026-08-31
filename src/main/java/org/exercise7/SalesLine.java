33f472cab5b468a5302dea14cf01faadd89befeb
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-31 15:18:26 -0500

    feat/ implementation of getLineTotal method

--- src/main/java/org/exercise7/SalesLine.java
+++ src/main/java/org/exercise7/SalesLine.java
@@ -1,4 +1,10 @@
 package org.exercise7;
 
-public record SalesLine() {
+import java.math.BigDecimal;
+
+public record SalesLine(Product product, int quantity) {
+
+    public BigDecimal getLineTotal(){
+        return (product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+    }
 }
