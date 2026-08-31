46a5185c9eaf79aa3e2c5987a199eb1566c8685a
Colmenarez <dcolmenarezr@unicartagena.edu.co>
2026-08-31 15:25:19 -0500

    Revert "Feature/logic of sales line"

--- src/main/java/org/exercise7/SalesLine.java
+++ src/main/java/org/exercise7/SalesLine.java
@@ -1,10 +1,4 @@
 package org.exercise7;
 
-import java.math.BigDecimal;
-
-public record SalesLine(Product product, int quantity) {
-
-    public BigDecimal getLineTotal(){
-        return (product.getPrice().multiply(BigDecimal.valueOf(quantity)));
-    }
+public record SalesLine() {
 }
