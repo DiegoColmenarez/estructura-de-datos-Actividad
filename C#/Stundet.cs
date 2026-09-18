eb774f50e0186d6c2cc799dee9ab309927c3b1cb
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-09-18 15:37:01 -0500

    feat/ add Student class

---
+++ C#/Stundet.cs
@@ -0,0 +1,22 @@
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Average { get; set; }
+
+    public Student(string name, int age, double average)
+    {
+    this.name = name;
+    this.age = age;
+    this.average = average;
+    }
+    public void ShowInfo()
+    {
+     Console.WriteLine($"{name} | Age: {age} | Average: {average}");
+    }
+
+    public void SetAverage(double newAverage)
+    {
+     this.average = newAverage;
+    }
+}
